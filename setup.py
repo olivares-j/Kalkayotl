08ec4f28e689cd109c17c91c070f314e8f1d0f46
olivares-j <avijer@gmai.com>
2018-04-26 13:55:28 +0200

    Adding installation dependencies

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Kalkayotl',
     version='0.1.0',
     author='Javier Olivares',
     author_email='javier.olivares-romero@u-bordeaux.fr',
-    packages=['code'],
+    packages=['kalkayotl'],
     url='http://perso.astrophy.u-bordeaux.fr/JOlivares/kalkayotl/index.html',
     license='COPYING',
     description='Simple parallax to distance converter',
     long_description=open('README.md').read(),
     install_requires=[
-        "emcee = 2.2.1",
-        "numpy >= 1.14",
-        "scipy >= 1.0",
-        "numpy >= 1.14",
-        "matplotlib  >= 2.1.1",
-        "pandas      >= 0.22",
-        "progressbar >= 2.3",
-        "astroML     >= 0.3"
+        'emcee>=2.2.1',
+        'numpy>=1.14',
+        'scipy>=1.0',
+        'numpy>=1.14',
+        'matplotlib>=2.1.1',
+        'pandas>=0.22',
+        'progressbar>=2.3',
+        'astroML>=0.3'
     ],
+    zip_safe=False
 )
