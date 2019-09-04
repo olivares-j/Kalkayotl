ed9c2922136ccb0c2d97cf170f8caedc68c422e4
olivares-j <avijer@gmail.com>
2019-09-04 11:08:10 +0200

    Adds Vasiliev+2018 correlation function

--- Code/Functions.py
+++ Code/Functions.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 
@@ -18,27 +19,52 @@ def AngularSeparation(a):
 
 	return A
 
-def CovariancePM(a):
+def CovariancePM(a,case="Lindegren+2018"):
 	'''
-	Assumes that the covariance is given by Eq. 18 of Lindegren et al. 2018.
+	Covariance matrix of the proper motions.
 	microarcsec^2 -> 1e-6 mas^2
 	Input in degrees.
 	Output in [mas/yr]^2
 	'''
+	if case == "Lindegren+2018":
+		'''
+		Assumes that the covariance is given by Eq. 18 of Lindegren et al. 2018.
+		'''
+		result = 0.0008*np.exp(-a/20.0)
 
-	result = (800*1e-6)*np.exp(-a/20.0)
+	elif case == "Vasiliev+2018":
+		'''
+		Assumes that the covariance is given by Eq. 1 of Vasiliev et al. 2018.
+		'''
+		result = 0.0008*np.exp(-a/20.0) + 0.004*np.sinc((a/0.5)+0.25)
+
+	else:
+		sys.exit("Correlation reference not valid!")
+
 
 	return result
 
-def CovarianceParallax(a):
+def CovarianceParallax(a,case="Lindegren+2018"):
 	'''
-	Assumes that the covariance is given by Eq. 16 of Lindegren et al. 2018.
+	Covariance matrix of the parallax
 	microarcsec^2 -> 1e-6 mas^2
 	Input in degrees.
 	Output in mas^2
 	'''
+	if case == "Lindegren+2018":
+		'''
+		Assumes that the covariance is given by Eq. 16 of Lindegren et al. 2018.
+		'''
+		result = 0.000285*np.exp(-a/14.0)
 
-	result = (285*1e-6)*np.exp(-a/14.0)
+	elif case == "Vasiliev+2018":
+		'''
+		Assumes that the covariance is given by Vasiliev et al. 2018.
+		'''
+		result = 0.0003*np.exp(-a/20.0) + 0.002*np.sinc((a/0.5)+0.25)
+
+	else:
+		sys.exit("Correlation reference not valid!")
 
 	return result
 
