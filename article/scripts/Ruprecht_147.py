2c17109215fd1db1a9b89279257e8c2a82c7d8fb
olivares-j <avijer@gmail.com>
2019-11-05 12:53:45 +0100

    Minor

--- article/scripts/Ruprecht_147.py
+++ article/scripts/Ruprecht_147.py
@@ -95,7 +95,7 @@ file_data = dir_main + "Data/" + file_csv
 #-------------------------------------
 
 #--------- Chains and plots ----------
-dir_out    = dir_main + "Outputs/"
+dir_out    = dir_main + "Outputs/Real/"
 dir_case   = dir_out  + case +"/"
 #--------------------------------------
 #==================================================
@@ -150,17 +150,17 @@ for prior in list_of_prior:
 					quantiles=quantiles)
 	p1d.load_data(file_data,id_name=id_name)
 	p1d.setup()
-	# p1d.run(sample_iters=sample_iters,
-	# 		burning_iters=burning_iters,
-	# 		chains=2,cores=2
-	# 		# target_accept=0.95,
-	# 		)
+	p1d.run(sample_iters=sample_iters,
+			burning_iters=burning_iters,
+			chains=2,cores=2
+			# target_accept=0.95,
+			)
 
 	# #-------- Analyse chains --------------------------------
-	# p1d.load_trace(burning_iters=burning_iters)
-	# p1d.convergence()
-	# coords = {"flavour_1d_source_dim_0" : range(5)}
-	# p1d.plot_chains(dir_out,coords=coords)
-	# p1d.save_statistics(statistic=statistic) 
+	p1d.load_trace(burning_iters=burning_iters)
+	p1d.convergence()
+	coords = {"flavour_1d_source_dim_0" : range(5)}
+	p1d.plot_chains(dir_out,coords=coords)
+	p1d.save_statistics(statistic=statistic) 
 	p1d.evidence(M_samples=1000,dlogz=1.0,nlive=100,file=file_Z)
 #=======================================================================================
