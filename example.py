ecdae7d58f81816c913aacd05dba0292a6efd181
olivares-j <avijer@gmail.com>
2019-11-05 11:24:52 +0100

    Uncommenting evidence line

--- example.py
+++ example.py
@@ -81,7 +81,7 @@ list_of_prior = [
 
 #===================== Chains =================================
 #---------------- MCMC parameters  --------------------
-burning_iters   = 40000
+burning_iters   = 10000
 sample_iters    = 10000   # Number of iterations for the MCMC 
 
 
@@ -158,19 +158,21 @@ for prior in list_of_prior:
 					quantiles=quantiles)
 	p1d.load_data(file_data,id_name=id_name)
 	p1d.setup()
-	p1d.evidence(N_samples=100,M_samples=1000,dlogz=1.0,nlive=100,file=file_Z,plot=True)
-	
 	p1d.run(sample_iters=sample_iters,
 			burning_iters=burning_iters,
 			chains=2,cores=2,
 			# target_accept=0.95,
 			)
 
-	#-------- Analyse chains --------------------------------
+	# -------- Analyse chains --------------------------------
 	p1d.load_trace(burning_iters=burning_iters)
 	p1d.convergence()
 	coords = {"flavour_1d_source_dim_0" : range(5)}
 	p1d.plot_chains(dir_out,coords=coords)
-	p1d.save_statistics(dir_csv=dir_out,
-						statistic=statistic)
+	p1d.save_statistics(statistic=statistic)
+	p1d.save_samples()
+
+	#------ Evidence ------
+	# Comment if you do not want the evidence 
+	p1d.evidence(N_samples=100,M_samples=1000,dlogz=1.0,nlive=100,file=file_Z)
 #=======================================================================================
