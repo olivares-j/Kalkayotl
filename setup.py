ea3e496c0105b650ca477ed52398806875132e7a
olivares-j <avijer@gmail.com>
2020-09-24 15:33:30 +0200

    New version with tutorial

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='Kalkayotl',
-    version='0.9.0',
+    version='1.0',
     author='Javier Olivares',
     author_email='javier.olivares-romero@u-bordeaux.fr',
     packages=['kalkayotl'],
+    url='https://github.com/olivares-j/Kalkayotl',
     license='COPYING',
     description='Star distance inference code',
     long_description=open('README.md').read(),
