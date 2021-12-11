ea945ee2c43b3523d95e9d18cabca17ae2ee5844
Javier Olivares <avijer@gmail.com>
2021-12-11 22:11:12 +0000

    Tested 1D and 3D with McCluster

--- kalkayotl/EFF.py
+++ kalkayotl/EFF.py
@@ -56,9 +56,9 @@ def toCartesian(rho,dimension,random_state=None):
 	return samples
 
 #=============== EFF generator ===============================
-class univariate_eff_gen(rv_continuous):
+class EFF_1D(rv_continuous):
 	"Univariate EFF distribution"
-	""" This probability density function is defined for x>0"""
+	""" This probability density function is defined for all x"""
 	def _pdf(self,x,gamma):
 
 		cte = np.sqrt(np.pi)*gamma_function(0.5*(gamma-1.))/gamma_function(gamma/2.)
@@ -87,16 +87,34 @@ class univariate_eff_gen(rv_continuous):
 				sol = root_scalar(
 					lambda x : self._cdf(x,gamma=gamma) - u,
 					xtol=1e-6,
-					bracket=[-1000.,1000.],
+					bracket=[0.0,1000.],
 					method='bisect')
 			except Exception as e:
-				print(self._cdf(-1000.0,gamma),u,self._cdf(1000.0,gamma))
+				print(0.0,u,self._cdf(1000.0,gamma))
 				raise
 			v[i] = sol.root
 			sol  = None
 		return v.reshape(size)
+#===============================================================
 
-eff = univariate_eff_gen(name='EFF')
+#=============== EFF generator ===============================
+class EFF_1D_psd(rv_continuous):
+	"Univariate EFF distribution"
+	""" This probability density function is defined for 0<x<Rm"""
+	def _pdf(self,x,gamma):
+		y = 1 + self.b**2
+		a = (1.0/(gamma-2.0))*(1.0-(y**(1.0-0.5*gamma))) # Truncated at Rm
+		# a = 1.0/(gamma-2.0)                            #  No truncated
+		nx = (1. + x**2)**(-0.5*gamma)
+		return nx/a
+
+
+	def _cdf(self,x,gamma):
+		w  = 1. + x**2 
+		y  = 1. + self.b**2
+		a  = 1. - (w**(1.0-0.5*gamma))
+		b  = 1. - (y**(1.0-0.5*gamma))
+		return a/b
 #===============================================================
 
 #================== Multivariate EFF =============================
@@ -167,14 +185,13 @@ class multivariate_eff_gen(multi_rv_generic):
 
 		"""
 		dim, loc, scale, gamma = self._process_parameters(loc, scale, gamma)
+		assert dim == 3, "Only valid for 3D!"
 		x = self._process_quantiles(x, dim)
 		scale_info = _PSD(scale)
-		if dim == 3:
-			return self._logpdf3d(x,loc,scale_info.U,scale_info.log_pdet,gamma,dim)
-		else:
-			sys.exit("ERROR DIM NOT IMPLEMENTED")
+		
+		return self._logpdf(x,loc,scale_info.U,scale_info.log_pdet,gamma,dim)
 
-	def _logpdf3d(self, x, loc, U, log_pdet, gamma,dim):
+	def _logpdf(self, x, loc, U, log_pdet, gamma,dim):
 		# U is inverse Cholesky, so only one product U*x instead of sqrt(x*U*U.T*x)
 		quaddist = np.square(np.dot(x - loc,U)).sum(axis=-1)
 
@@ -186,7 +203,7 @@ class multivariate_eff_gen(multi_rv_generic):
 		
 		return log_d
 
-	def _cdf3d(self, x, rc, gamma):
+	def _cdf(self, x, rc, gamma):
 		# use only to generate random variates
 		a = x**2 + rc**2
 		hg = gamma_function(0.5*gamma)
@@ -197,11 +214,10 @@ class multivariate_eff_gen(multi_rv_generic):
 
 		up = 4.*(a**(-0.5*gamma))*hg*( b + c)
 		down = (gamma-3.)*(gamma-1.)*np.sqrt(np.pi)*(rc**3)*x*hg3
-
 		result = up/down
 		return result
 
-	def _rvs_r3d(self, rc,gamma, size,upper=[0.99,1e6]):
+	def _rvs(self, rc,gamma, size,upper=[0.99,1e6]):
 		#------ This rvs are standardized and are not meant to be used
 		#------- but in combination with rvs
 		#---------------------------------------------
@@ -213,13 +229,13 @@ class multivariate_eff_gen(multi_rv_generic):
 		for i,u in enumerate(us):
 			try:
 				sol = root_scalar(
-							lambda x : self._cdf3d(x,rc,gamma) - u,
+							lambda x : self._cdf(x,rc,gamma) - u,
 							xtol=1e-6,
-							bracket=[0.0,upper[1]],
+							bracket=[1e-6,upper[1]],
 							method='bisect')
 			except Exception as e:
-				print("Value outside range [{0},{1},{2}]".format(self._cdf3d(0.0,rc,gamma),
-					u,self._cdf3d(upper[1],rc,gamma)))
+				print("Value outside range [{0},{1},{2}]".format(self._cdf(0.0,rc,gamma),
+					u,self._cdf(upper[1],rc,gamma)))
 				raise
 			v[i] = sol.root
 			sol  = None
@@ -249,25 +265,17 @@ class multivariate_eff_gen(multi_rv_generic):
 		else:
 			rng = self._random_state
 		dim, loc, scale, gamma = self._process_parameters(loc, scale, gamma)
+		assert dim == 3, "Only valid for 3D!"
 		scale_info = _PSD(scale)
-		# rc = np.exp(log_pdet)
 
 		#------ Take samples from the distance -------------
-		if dim == 1 :
-			rho = eff.rvs(gamma=gamma,size=size)
-		elif dim == 3:
-			rho = self._rvs_r3d(rc=1.0, gamma=gamma, size=size)
-		else:
-			sys.exit("Dimension {0} not implemented".format(dim))
+		rho = self._rvs(rc=1.0, gamma=gamma, size=size)
 
 		#------ Samples from the angles -------
 		samples = toCartesian(rho,dim,random_state=rng).reshape(size,dim)
 
-		if dim > 1:
-			chol = np.linalg.cholesky(scale)
-			samples = np.dot(samples,chol)
-		else:
-			samples = samples*scale
+		chol = np.linalg.cholesky(scale)
+		samples = np.dot(samples,chol)
 
 		return loc + samples
 
@@ -361,17 +369,20 @@ class multivariate_eff_frozen(multi_rv_frozen):
 		"""
 		self._dist = multivariate_eff_gen(seed)
 		self.dim, self.loc, self.scale, self.gamma = self._dist._process_parameters(loc, scale, gamma)
+		assert self.dim == 3, "Only valid for 3D!"
 		self.scale_info = _PSD(self.scale)
 
 	def logpdf(self, x):
 		x = self._dist._process_quantiles(x, self.dim)
-		if self.dim == 3:
-			return self._dist._logpdf3d(x, self.loc, self.scale_info.U, self.scale_info.log_pdet, self.gamma, self.dim)
-		else:
-			sys.exit("error")
+
+		return self._dist._logpdf(x, self.loc, self.scale_info.U, self.scale_info.log_pdet, self.gamma, self.dim)
+
 
 	def pdf(self, x):
 		return np.exp(self.logpdf(x))
+
+	def cdf(self, x,rc,gamma):
+		return self._dist._cdf(x,rc,gamma)
 
 	def rvs(self, size=1, random_state=None):
 		"""
@@ -407,39 +418,52 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import scipy.stats as st
 
-def test_eff_1d(n=1000,r0=100.,rc=1.,gamma=5):
+def test_eff_1d_psd(n=1000,rc=1.,gamma=5.0,mc_file=None,bins=40):
 	# ----- Generate samples ---------
-	s = eff.rvs(loc=r0,scale=rc,gamma=gamma,size=n)
+	eff = EFF_1D_psd(name='EFF',a=0.0,b=20.0)
+	r = eff.rvs(scale=rc,gamma=gamma,size=n)
+
+	if mc_file is not None:
+		mc = pd.read_csv(mc_file,sep="\t",header=None,skiprows=1,
+					names=["Mass","X","Y","Z","U","V","W"])
+
+		pos = mc[["X","Y","Z"]].to_numpy()
+
+		s = np.sqrt(np.sum(pos**2,axis=1))
 
 	#------ grid ----------------------
-	range_dist = (r0-20*rc,r0+20*rc)
-	x = np.linspace(range_dist[0],range_dist[1],1000)
-	y = eff.pdf(x,loc=r0,scale=rc,gamma=gamma)
-	z = eff.cdf(x,loc=r0,scale=rc,gamma=gamma)
+	xlim = (0.0,30*rc)
+	x = np.linspace(xlim[0],xlim[1],1000)
+	y = eff.pdf(x,scale=rc,gamma=gamma)
+	z = eff.cdf(x,scale=rc,gamma=gamma)
 
 	pdf = PdfPages(filename="Test_EFF.pdf")
-	plt.figure(0)
-	plt.hist(s,bins=100,range=range_dist,density=True,histtype='step',color="grey",label="1D Samples")
-	plt.plot(x,y,color="black",label="1D PDF")
-	plt.xlim(range_dist)
+	plt.figure()
+	plt.hist(r,bins=bins,density=True,histtype='step',label="EFF_1D_psd")
+	if mc_file is not None:
+		plt.hist(s,bins=bins,density=True,histtype='step',label="McCluster")
 	plt.yscale('log')
 	plt.legend()
-	
-	#-------------- Save fig --------------------------
 	pdf.savefig(bbox_inches='tight')
-	plt.close(0)
+	plt.close()
+
+	plt.figure()
+	plt.plot(x,y,color="black",label="PDF")
+	plt.yscale('log')
+	plt.legend()
+	pdf.savefig(bbox_inches='tight')
+	plt.close()
 
 	plt.figure(1)
 	plt.plot(x,z,color="black",label="CDF")
+	plt.yscale('log')
 	plt.legend()
-	
-	#-------------- Save fig --------------------------
 	pdf.savefig(bbox_inches='tight')
 	plt.close(1)
 	
 	pdf.close()
 
-def test_eff_3d(n=100,r0=0.,rc=1.,gamma=5.0,mc_file=None):
+def test_eff_3d(n=1000,r0=0.,rc=1.,gamma=5.0,mc_file=None):
 	loc = np.array([r0,0.0,0.0])
 	scl = np.array([[rc,0.0,0.0],
 					[0.0,rc,0.0],
@@ -448,28 +472,37 @@ def test_eff_3d(n=100,r0=0.,rc=1.,gamma=5.0,mc_file=None):
 	if mc_file is not None:
 		mc = pd.read_csv(mc_file,sep="\t",header=None,skiprows=1,
 					names=["Mass","X","Y","Z","U","V","W"])
-
-		pos = mc[["X","Y","Z"]].to_numpy()
-
-		r_mc = np.sqrt(np.sum(pos**2,axis=1))
+		s_mc = mc[["X","Y","Z"]].to_numpy()
+		r_mc = np.sqrt(np.sum(s_mc**2,axis=1))
 
 	# ----- Generate samples ---------
+	ue = EFF_1D_psd(name='EFF',a=0.0,b=20.0)
 	me = mveff(loc=loc,scale=scl,gamma=gamma)
-	ue = eff(loc=r0,scale=rc,gamma=gamma)
 	s_mv = me.rvs(size=n)
-	s_uv = ue.rvs(size=n)
 	r_mv = np.sqrt(np.sum(s_mv**2,axis=1))
+	# print(s_mv)
 
 	#-------- Density ---------------
-	z = me.pdf(s_mv)
+	z_mv = me.pdf(s_mv)
+	z_mc = me.pdf(s_mc)
+	z_mv_1d = ue.pdf(r_mv,scale=rc,gamma=gamma)
+	z_mc_1d = ue.pdf(r_mc,scale=rc,gamma=gamma)
 
 	#------ grid ----------------------
 	range_dist = (0,10)
-	x = np.linspace(range_dist[0],range_dist[1],1000)
-	y = ue.cdf(x)
-	c = mveff._cdf3d(x, rc=rc, gamma=gamma)
+	x = np.linspace(0.0,20.0,100)
+	y = ue.cdf(x,scale=rc,gamma=gamma)	
+	c = me.cdf(x,rc=rc, gamma=gamma)
 
 	pdf = PdfPages(filename="Test_EFF_3D.pdf")
+
+	#-------------- Distance -------------------
+	plt.figure(0)
+	plt.hist(r_mv,bins=100,density=True,histtype='step',label="Samples")
+	plt.hist(r_mc,bins=100,density=True,histtype='step',label="McCluster")
+	plt.legend()
+	pdf.savefig(bbox_inches='tight')
+	plt.close(0)
 
 	plt.figure(0)
 	plt.plot(x,c,label="3D CDF")
@@ -478,52 +511,56 @@ def test_eff_3d(n=100,r0=0.,rc=1.,gamma=5.0,mc_file=None):
 	pdf.savefig(bbox_inches='tight')
 	plt.close(0)
 
-	#-------------- Distance -------------------
 	plt.figure(0)
-	plt.hist(np.abs(s_uv),bins=100,density=True,histtype='step',label="1D Samples")
-	plt.hist(r_mv,bins=100,density=True,histtype='step',label="3D Samples")
-	plt.hist(r_mc,bins=100,density=True,histtype='step',label="MC Samples")
+	plt.scatter(r_mc,z_mc,marker="s",label="McCluster")
+	plt.scatter(r_mv,z_mv,marker=".",label="Samples")
+	plt.scatter(r_mc,z_mc_1d,marker="s",label="McCluster 1D")
+	plt.scatter(r_mv,z_mv_1d,marker=".",label="Samples 1D")
 	plt.legend()
+	plt.xlabel("R")
+	plt.ylabel("Density")
 	pdf.savefig(bbox_inches='tight')
 	plt.close(0)
 
-	# plt.figure(0)
-	# plt.scatter(s_mv[:,0],s_mv[:,1],s=1,c=z,label="3D Samples")
-	# plt.legend()
-	# plt.xlabel("X")
-	# plt.ylabel("Y")
-	# plt.gca().set_aspect('equal', adjustable='box')
-	# #-------------- Save fig --------------------------
-	# pdf.savefig(bbox_inches='tight')
-	# plt.close(0)
+	plt.figure(0)
+	plt.scatter(s_mv[:,0],s_mv[:,1],s=1,c=z_mv,marker=".",label="Samples")
+	plt.scatter(s_mc[:,0],s_mc[:,1],s=1,c=z_mc,marker="s",label="McCluster")
+	plt.legend()
+	plt.xlabel("X")
+	plt.ylabel("Y")
+	plt.gca().set_aspect('equal', adjustable='box')
+	pdf.savefig(bbox_inches='tight')
+	plt.close(0)
 
-	# plt.figure(0)
-	# plt.scatter(s_mv[:,2],s_mv[:,0],s=1,c=z,label="3D Samples")
-	# plt.legend()
+	plt.figure(0)
+	plt.scatter(s_mv[:,2],s_mv[:,0],s=1,c=z_mv,marker=".",label="Samples")
+	plt.scatter(s_mc[:,2],s_mc[:,0],s=1,c=z_mc,marker="s",label="McCluster")
+	plt.legend()
+	plt.xlabel("X")
+	plt.ylabel("Z")
+	plt.gca().set_aspect('equal', adjustable='box')
+	pdf.savefig(bbox_inches='tight')
+	plt.close(0)
 
-	# plt.xlabel("X")
-	# plt.ylabel("Z")
-	# plt.gca().set_aspect('equal', adjustable='box')
-	# #-------------- Save fig --------------------------
-	# pdf.savefig(bbox_inches='tight')
-	# plt.close(0)
-
-	# #-------------- Distance -------------------
-	# plt.figure(0)
-	# # plt.hist(rho,bins=100,density=True,histtype='step',label="Rho")
-	# plt.hist(s_mv[:,0],bins=100,density=True,histtype='step',label="X")
-	# plt.hist(s_mv[:,1],bins=100,density=True,histtype='step',label="Y")
-	# plt.hist(s_mv[:,2],bins=100,density=True,histtype='step',label="Z")
-	# plt.yscale('log')
-	# plt.ylim(bottom=1e-5)
-	# plt.legend()
-	# pdf.savefig(bbox_inches='tight')
-	# plt.close(0)
+	#-------------- Distance -------------------
+	plt.figure(0)
+	plt.hist(s_mv[:,0],bins=100,density=True,histtype='step',label="X Samples")
+	plt.hist(s_mv[:,1],bins=100,density=True,histtype='step',label="Y Samples")
+	plt.hist(s_mv[:,2],bins=100,density=True,histtype='step',label="Z Samples")
+	plt.hist(s_mc[:,0],bins=100,density=True,histtype='step',label="X McCluster")
+	plt.hist(s_mc[:,1],bins=100,density=True,histtype='step',label="Y McCluster")
+	plt.hist(s_mc[:,2],bins=100,density=True,histtype='step',label="Z McCluster")
+	plt.yscale('log')
+	plt.ylim(bottom=1e-5)
+	plt.legend()
+	pdf.savefig(bbox_inches='tight')
+	plt.close(0)
 
 	
 	pdf.close()
 
 if __name__ == "__main__":
 
-	# test_eff_1d()
-	test_eff_3d(mc_file="/home/jolivares/Repos/Amasijo/Data/EFF_n1000_r1_g5.txt")
+	mc_file = "/home/jolivares/Repos/Amasijo/Data/EFF_n1000_r1_g5.txt"
+	# test_eff_1d_psd(n=1000,rc=1.,gamma=5,mc_file=mc_file)
+	test_eff_3d(mc_file=mc_file)
