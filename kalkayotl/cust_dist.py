e207f9056c53db2c3b21b653337825f6f80faf72
cosminmarina <cosmin.marina@edu.uah.es>
2023-08-01 09:30:35 +0200

    fix: solved random and logp test
    tests correct for NumPy an PyTensor versions with X/Z multivariate

--- kalkayotl/cust_dist.py
+++ kalkayotl/cust_dist.py
@@ -12,30 +12,22 @@ import arviz as az
 
 ################################## Tails Dist ####################################
 def tails_logp(value, mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r):
-    #shape = np.shape(value)
-    new_value = tt.zeros_like(value[:,0])
-    #for i in range(shape[0]):
-    for j in range(3):
-        if j!=1:
-            new_value = new_value + pm.logp(pm.Normal.dist(mu=mu[j], sigma=chol[j][j]), value[:,j])
-            #new_value = tt.set_subtensor(new_value[j], value_j)
-        else:
-            value_l = value[:,j][value[:,j] < mu[j]]
-            value_r = value[:,j][value[:,j] >= mu[j]]
-            #left_res = weight*pm.logp(pm.Gamma.dist(alpha=alpha_l, beta=beta_l),-(value_l - mu[j]))
-            #right_res = (1-weight)*pm.logp(pm.Gamma.dist(alpha=alpha_r, beta=beta_r),value_r - mu[j])
-            left_res = new_value[value[:,j] < mu[j]] + weight*pm.logp(pm.Gamma.dist(alpha=alpha_l, beta=beta_l),-(value_l - mu[j]))
-            right_res = new_value[value[:,j] >= mu[j]] + (1-weight)*pm.logp(pm.Gamma.dist(alpha=alpha_r, beta=beta_r),value_r - mu[j])
-            new_value = tt.set_subtensor(new_value[value[:,j] < mu[j]], left_res)
-            new_value = tt.set_subtensor(new_value[value[:,j] >= mu[j]], right_res)
-    return new_value
+    value_aux = tt.zeros_like(value[:,0])
+    value_aux = value_aux + pm.logp(pm.MvNormal.dist(mu=mu[::2], cov=chol), value[:,::2])
+    value_l = value[:,1][value[:,1] < mu[1]]
+    value_r = value[:,1][value[:,1] >= mu[1]]
+    left_res = value_aux[value[:,1] < mu[1]] + weight*pm.logp(pm.Gamma.dist(alpha=alpha_l, beta=beta_l),-(value_l - mu[1]))
+    right_res = value_aux[value[:,1] >= mu[1]] + (1-weight)*pm.logp(pm.Gamma.dist(alpha=alpha_r, beta=beta_r),value_r - mu[1])
+    value_aux = tt.set_subtensor(value_aux[value[:,1] < mu[1]], left_res)
+    value_aux = tt.set_subtensor(value_aux[value[:,1] >= mu[1]], right_res)
+    return value_aux
 
 def tails_random(mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r, rng=None, size=None):
     size = list(size)
     res = tt.zeros(size)
     res_xz = rng.multivariate_normal(mean=mu[::2], cov=chol, size=size[0])
-    res = tt.set_subtensor(res_xz[:,0], res_xz[:,0])
-    res = tt.set_subtensor(res_xz[:,2], res_xz[:,1])
+    res = tt.set_subtensor(res[:,0], res_xz[:,0])
+    res = tt.set_subtensor(res[:,2], res_xz[:,1])
     size_y_l = size[0]
     size_y_r = size[0]
     size_y_l = int(weight*size_y_l)
@@ -44,21 +36,6 @@ def tails_random(mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r, rng=None, s
     right_res = mu[1] + rng.gamma(shape=alpha_r, scale=1/beta_r, size=size_y_r)
     res_y = tt.concatenate([left_res, right_res],axis=0)
     res = tt.set_subtensor(res[:,1], res_y)
-    # dims = size[1]
-    # size = size[0]
-    # for j in range(dims):
-    #     if j!=1:
-    #         res_j = rng.normal(loc=mu[j], scale=chol[j][j], size=size)
-    #         res = tt.set_subtensor(res[:,j], res_j)
-    #     else:
-    #         size_y_l = size
-    #         size_y_r = size
-    #         size_y_l = int(weight*size_y_l)
-    #         size_y_r = int(size-size_y_l)
-    #         left_res = mu[j] - rng.gamma(shape=alpha_l, scale=1/beta_l, size=size_y_l)
-    #         right_res = mu[j] + rng.gamma(shape=alpha_r, scale=1/beta_r, size=size_y_r)
-    #         res_j = tt.concatenate([left_res, right_res],axis=0)
-    #         res = tt.set_subtensor(res[:,j], res_j)
     return res
 
 class TailsDist():
@@ -70,22 +47,12 @@ class TailsDist():
         
 
 def np_tails_logp(value, mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r):
-    #shape = np.shape(value)
-    #for i in range(shape[0]):
     value_aux = np.zeros_like(value[:,0])
     value_aux = value_aux + sp.stats.multivariate_normal.logpdf(x=value[:,::2], mean=mu[::2], cov=chol)
     value_l = value[:,1][value[:,1] < mu[1]].copy()
     value_r = value[:,1][value[:,1] >= mu[1]].copy()
     value_aux[value[:,1] < mu[1]] = value_aux[value[:,1] < mu[1]] + weight*sp.stats.gamma.logpdf(x=-(value_l - mu[1]), a=alpha_l, scale=1/beta_l)
     value_aux[value[:,1] >= mu[1]] = value_aux[value[:,1] >= mu[1]] + (1-weight)*sp.stats.gamma.logpdf(x=value_r - mu[1], a=alpha_r, scale=1/beta_r)
-    # for j in range(3):
-    #     if j!=1:
-    #         value_aux = value_aux + sp.stats.norm.logpdf(x=value[:,j], loc=mu[j], scale=chol[j][j])
-    #     else:
-    #         value_l = value[:,j][value[:,j] < mu[j]].copy()
-    #         value_r = value[:,j][value[:,j] >= mu[j]].copy()
-    #         value_aux[value[:,j] < mu[j]] = value_aux[value[:,j] < mu[j]] + weight*sp.stats.gamma.logpdf(x=-(value_l - mu[j]), a=alpha_l, scale=1/beta_l)
-    #         value_aux[value[:,j] >= mu[j]] = value_aux[value[:,j] >= mu[j]] + (1-weight)*sp.stats.gamma.logpdf(x=value_r - mu[j], a=alpha_r, scale=1/beta_r)
     return value_aux
 
 def np_tails_random(mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r, rng=None, size=None):
@@ -101,19 +68,6 @@ def np_tails_random(mu, chol, weight, alpha_l, alpha_r, beta_l, beta_r, rng=None
     left_res = mu[1] - rng.gamma(shape=alpha_l, scale=1/beta_l, size=size_y_l)
     right_res = mu[1] + rng.gamma(shape=alpha_r, scale=1/beta_r, size=size_y_r)
     res[:,1] = np.concatenate((left_res, right_res), axis=0)
-    # dims = size[1]
-    # size = size[0]
-    # for j in range(dims):
-    #     if j!=1:
-    #         res[:,j] = rng.normal(loc=mu[j], scale=chol[j][j], size=size)
-    #     else:
-    #         size_y_l = size
-    #         size_y_r = size
-    #         size_y_l = int(weight*size_y_l)
-    #         size_y_r = int(size-size_y_l)
-    #         left_res = mu[j] - rng.gamma(shape=alpha_l, scale=1/beta_l, size=size_y_l)
-    #         right_res = mu[j] + rng.gamma(shape=alpha_r, scale=1/beta_r, size=size_y_r)
-    #         res[:,j] = np.concatenate((left_res, right_res), axis=0)
     return res
 
 def test_tails_random_np(verbose:bool=False, confidence:float=0.90):
@@ -121,6 +75,7 @@ def test_tails_random_np(verbose:bool=False, confidence:float=0.90):
     import seaborn as sns
     sns.set()
     from scipy import stats
+    import pandas as pd
     mu = np.random.randint(-10,10, size=3)
     chol = np.eye(2)*np.random.random(size=2)*10
     weight = np.random.random()
@@ -131,21 +86,11 @@ def test_tails_random_np(verbose:bool=False, confidence:float=0.90):
     size = (1000,3)
     rand_extr = np_tails_random(mu=mu, chol=chol, weight=weight, alpha_l=alpha_l, alpha_r=alpha_r, beta_l=beta_l, beta_r=beta_r, rng=np.random.default_rng(np.random.randint(1000)), size=size)
     print("========= Testing NP Random of TailsDist ===============")
-    print("------------------------ X/Z ---------------------------")
-    print(np.shape(rand_extr))
-    rand_extr_x = rand_extr[:,0]
     rand_extr_xz = rand_extr[:,::2]
-    print(np.shape(rand_extr_xz))
-    print(np.shape(rand_extr[:,0]))
-    ks_test_xz = stats.kstest(rand_extr_xz, stats.multivariate_normal(mean=mu[::2], cov=chol).cdf)
-    ks_test_x = stats.kstest(rand_extr_xz[:,0], stats.norm(loc=mu[0], scale=chol[0][0]).cdf)
-    ks_test_x2 = stats.kstest(rand_extr_x, stats.norm(loc=mu[0], scale=chol[0][0]).cdf)
-    ks_test_z = stats.kstest(rand_extr_xz[:,1], stats.norm(loc=mu[2], scale=chol[1][1]).cdf)
-    print(ks_test_xz.pvalue)
-    print(ks_test_x.pvalue)
-    print(ks_test_x2.pvalue)
-    print(ks_test_z.pvalue)
-    #np.testing.assert_(ks_test_xz.pvalue > (1 - confidence), msg='Fail at X and Z Coords')
+    sp_rand_extr_xz = stats.multivariate_normal.rvs(mean=mu[::2], cov=chol, size=(size[0]))
+    print("------------------------ X ---------------------------")
+    ks_test_x = stats.ks_2samp(rand_extr_xz[:,0], sp_rand_extr_xz[:,0])
+    np.testing.assert_(ks_test_x.pvalue > (1 - confidence), msg='Fail at X Coords')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     print("------------------------- Y ----------------------------")
@@ -154,36 +99,47 @@ def test_tails_random_np(verbose:bool=False, confidence:float=0.90):
     rand_extr_r = rand_extr_y[rand_extr_y >= mu[1]] - mu[1]
     print("-------------------- Left Gamma ------------------------")
     ks_test_l = stats.kstest(rand_extr_l, stats.gamma(a=alpha_l, scale=1/beta_l).cdf)
-    print(ks_test_l.pvalue)
     np.testing.assert_(ks_test_l.pvalue > (1 - confidence), msg='Fail at Left Gamma of Y Coord')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     print("-------------------- Right Gamma -----------------------")
     ks_test_r = stats.kstest(rand_extr_r, stats.gamma(a=alpha_r, scale=1/beta_r).cdf)
-    print(ks_test_r.pvalue)
     np.testing.assert_(ks_test_r.pvalue > (1 - confidence), msg='Fail at Right Gamma of Y Coord')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     print("------------------------- Z ----------------------------")
-    rand_extr_z = rand_extr[:,2]
-    #ks_test_z = stats.kstest(rand_extr_z, stats.norm(loc=mu[2], scale=chol[2][2]).cdf)
-    #np.testing.assert_(ks_test_z.pvalue > (1 - confidence), msg='Fail at Z Coord')
+    ks_test_z = stats.ks_2samp(rand_extr_xz[:,1], sp_rand_extr_xz[:,1])
+    np.testing.assert_(ks_test_z.pvalue > (1 - confidence), msg='Fail at Z Coord')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     if verbose:
         fig, axes = plt.subplots(2,2)
-        sns.histplot(rand_extr_x, kde=True, ax=axes[0,0])
+        sns.histplot(rand_extr_xz[:,0], kde=True, ax=axes[0,0])
         axes[0,0].set_title('X')
         sns.histplot(rand_extr_y, kde=True, ax=axes[0,1])
         axes[0,1].set_title('Y')
-        sns.histplot(rand_extr_z, kde=True, ax=axes[1,0])
+        sns.histplot(rand_extr_xz[:,1], kde=True, ax=axes[1,0])
         axes[1,0].set_title('Z')
         ax = fig.add_subplot(2, 2, 4, projection='3d')
-        ax.scatter3D(rand_extr_y, rand_extr_x, rand_extr_z)
+        ax.scatter3D(rand_extr_y, rand_extr_xz[:,0], rand_extr_xz[:,1])
         ax.set_xlabel('y')
         ax.set_ylabel('x')
         ax.set_zlabel('z')
         ax.set_title('Scatter')
+        plt.show()
+        zeros = np.zeros_like(rand_extr_xz[:,0])
+        ones = np.ones_like(sp_rand_extr_xz[:,0])
+        rand_extr_xz = np.insert(rand_extr_xz, np.shape(rand_extr_xz)[1], zeros, axis=1)
+        sp_rand_extr_xz = np.insert(sp_rand_extr_xz, np.shape(sp_rand_extr_xz)[1], ones, axis=1)
+        all_rand_extr_xz = np.insert(rand_extr_xz, np.shape(rand_extr_xz)[0], sp_rand_extr_xz, axis=0)
+        df = pd.DataFrame(all_rand_extr_xz, columns=['X', 'Z', 'source'])
+        sns.jointplot(
+            data=df,
+            x='X',
+            y='Z',
+            hue='source',
+            kind='kde'
+        )
         plt.show()
 
 def test_tails_logp_np():
@@ -202,17 +158,14 @@ def test_tails_logp_np():
     print("========= Testing NP Logp of TailsDist =================")
     rand_extr_xz = rand_extr[:,::2]
     rand_extr_y = rand_extr[:,1]
-    #rand_extr_z = rand_extr[:,2]
     rand_extr_l = rand_extr_y[rand_extr_y < mu[1]]
     rand_extr_r = rand_extr_y[rand_extr_y >= mu[1]]
     rand_extr_l = -(rand_extr_l - mu[1])
     rand_extr_r = rand_extr_r - mu[1]
     sp_log_extr = np.zeros_like(rand_extr_y)
     sp_log_extr = sp_log_extr + stats.multivariate_normal.logpdf(rand_extr_xz, mean=mu[::2], cov=chol)
-    #sp_log_extr = sp_log_extr + stats.norm.logpdf(rand_extr_x, loc=mu[0], scale=chol[0][0])
     sp_log_extr[rand_extr_y < mu[1]] = sp_log_extr[rand_extr_y < mu[1]] + weight*stats.gamma.logpdf(rand_extr_l, a=alpha_l, scale=1/beta_l)
     sp_log_extr[rand_extr_y >= mu[1]] = sp_log_extr[rand_extr_y >= mu[1]] + (1-weight)*stats.gamma.logpdf(rand_extr_r, a=alpha_r, scale=1/beta_r)
-    #sp_log_extr = sp_log_extr + stats.norm.logpdf(rand_extr_z, loc=mu[2], scale=chol)
     np.testing.assert_allclose(sp_log_extr, log_extr, rtol=1e-10, atol=0, err_msg='Fail at NP Logp')
     print("                          OK                            ")
     print("--------------------------------------------------------")
@@ -222,8 +175,9 @@ def test_tails_random(verbose:bool=False, confidence:float=0.90):
     import seaborn as sns
     sns.set()
     from scipy import stats
+    import pandas as pd
     mu = np.random.randint(-10,10, size=3)
-    chol = np.eye(3)*np.random.random(size=3)
+    chol = np.eye(2)*np.random.random(size=2)
     weight = np.random.random()
     alpha_l = np.random.randint(5, 15)
     alpha_r = np.random.randint(5, 15)
@@ -233,10 +187,11 @@ def test_tails_random(verbose:bool=False, confidence:float=0.90):
     print("========== Testing Random of TailsDist =================")
     f = pytensor.function([], tails_random(mu=mu, chol=chol, weight=weight, alpha_l=alpha_l, alpha_r=alpha_r, beta_l=beta_l, beta_r=beta_r, rng=np.random.default_rng(np.random.randint(1000)), size=size))
     rand_extr = f()
+    rand_extr_xz = rand_extr[:,::2]
+    sp_rand_extr_xz = stats.multivariate_normal.rvs(mean=mu[::2], cov=chol, size=(size[0]))
     print("------------------------- X ----------------------------")
-    rand_extr_x = rand_extr[:,0]
-    ks_test_x = stats.kstest(rand_extr_x, stats.norm(loc=mu[0], scale=chol[0][0]).cdf)
-    np.testing.assert_(ks_test_x.pvalue > (1 - confidence), msg='Fail at X Coord')
+    ks_test_x = stats.ks_2samp(rand_extr_xz[:,0], sp_rand_extr_xz[:,0])
+    np.testing.assert_(ks_test_x.pvalue > (1 - confidence), msg='Fail at X Coords')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     print("------------------------- Y ----------------------------")
@@ -254,32 +209,45 @@ def test_tails_random(verbose:bool=False, confidence:float=0.90):
     print("                          OK                            ")
     print("--------------------------------------------------------")
     print("------------------------- Z ----------------------------")
-    rand_extr_z = rand_extr[:,2]
-    ks_test_z = stats.kstest(rand_extr_z, stats.norm(loc=mu[2], scale=chol[2][2]).cdf)
+    ks_test_z = stats.ks_2samp(rand_extr_xz[:,1], sp_rand_extr_xz[:,1])
     np.testing.assert_(ks_test_z.pvalue > (1 - confidence), msg='Fail at Z Coord')
     print("                          OK                            ")
     print("--------------------------------------------------------")
     if verbose:
         fig, axes = plt.subplots(2,2)
-        sns.histplot(rand_extr[:,0], kde=True, ax=axes[0,0])
+        sns.histplot(rand_extr_xz[:,0], kde=True, ax=axes[0,0])
         axes[0,0].set_title('X')
-        sns.histplot(rand_extr[:,1], kde=True, ax=axes[0,1])
+        sns.histplot(rand_extr_y, kde=True, ax=axes[0,1])
         axes[0,1].set_title('Y')
-        sns.histplot(rand_extr[:,2], kde=True, ax=axes[1,0])
+        sns.histplot(rand_extr_xz[:,1], kde=True, ax=axes[1,0])
         axes[1,0].set_title('Z')
         ax = fig.add_subplot(2, 2, 4, projection='3d')
-        ax.scatter3D(rand_extr[:,1], rand_extr[:,0], rand_extr[:,2])
+        ax.scatter3D(rand_extr_y, rand_extr_xz[:,0], rand_extr_xz[:,1])
         ax.set_xlabel('y')
         ax.set_ylabel('x')
         ax.set_zlabel('z')
         ax.set_title('Scatter')
+        plt.show()
+        zeros = np.zeros_like(rand_extr_xz[:,0])
+        ones = np.ones_like(sp_rand_extr_xz[:,0])
+        rand_extr_xz = np.insert(rand_extr_xz, np.shape(rand_extr_xz)[1], zeros, axis=1)
+        sp_rand_extr_xz = np.insert(sp_rand_extr_xz, np.shape(sp_rand_extr_xz)[1], ones, axis=1)
+        all_rand_extr_xz = np.insert(rand_extr_xz, np.shape(rand_extr_xz)[0], sp_rand_extr_xz, axis=0)
+        df = pd.DataFrame(all_rand_extr_xz, columns=['X', 'Z', 'source'])
+        sns.jointplot(
+            data=df,
+            x='X',
+            y='Z',
+            hue='source',
+            kind='kde'
+        )
         plt.show()
 
 def test_tails_logp():
     from scipy import stats
     import matplotlib.pyplot as plt
     mu = np.random.randint(-10,10, size=3)
-    chol = np.eye(3)*np.random.random(size=3)*10
+    chol = np.eye(2)*np.random.random(size=2)*10
     weight = np.random.random()
     alpha_l = np.random.randint(5, 15)
     alpha_r = np.random.randint(5, 15)
@@ -291,18 +259,16 @@ def test_tails_logp():
     fl = pytensor.function([], tails_logp(value=rand_extr, mu=mu, chol=chol, weight=weight, alpha_l=alpha_l, alpha_r=alpha_r, beta_l=beta_l, beta_r=beta_r))
     log_extr = fl()
     print("========== Testing Logp of TailsDist ===================")
-    rand_extr_x = rand_extr[:,0]
+    rand_extr_xz = rand_extr[:,::2]
     rand_extr_y = rand_extr[:,1]
     rand_extr_l = rand_extr_y[rand_extr_y < mu[1]]
     rand_extr_r = rand_extr_y[rand_extr_y >= mu[1]]
     rand_extr_l = -(rand_extr_l - mu[1])
     rand_extr_r = rand_extr_r - mu[1]
-    rand_extr_z = rand_extr[:,2]
-    sp_log_extr = np.zeros_like(rand_extr_x)
-    sp_log_extr = sp_log_extr + stats.norm.logpdf(rand_extr_x, loc=mu[0], scale=chol[0][0])
+    sp_log_extr = np.zeros_like(rand_extr_y)
+    sp_log_extr = sp_log_extr + stats.multivariate_normal.logpdf(rand_extr_xz, mean=mu[::2], cov=chol)
     sp_log_extr[rand_extr_y < mu[1]] = sp_log_extr[rand_extr_y < mu[1]] + weight*stats.gamma.logpdf(rand_extr_l, a=alpha_l, scale=1/beta_l)
     sp_log_extr[rand_extr_y >= mu[1]] = sp_log_extr[rand_extr_y >= mu[1]] + (1-weight)*stats.gamma.logpdf(rand_extr_r, a=alpha_r, scale=1/beta_r)
-    sp_log_extr = sp_log_extr + stats.norm.logpdf(rand_extr_z, loc=mu[2], scale=chol[2][2])
     np.testing.assert_allclose(sp_log_extr, log_extr, rtol=1e-10, atol=0, err_msg='Fail at Logp')
     print("                          OK                            ")
     print("--------------------------------------------------------")
@@ -310,7 +276,7 @@ def test_tails_logp():
 if __name__ == "__main__":
     verbose = True
     #test_tails_random_np(verbose=verbose, confidence=0.80)
-    test_tails_logp_np()
+    #test_tails_logp_np()
     #test_tails_random(verbose=verbose, confidence=0.80)
     #test_tails_logp()
     # with pm.Model() as m:
