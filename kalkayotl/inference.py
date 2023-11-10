c7af0f3984b9f572eb89568f9c43d7e6891e674f
olivares-j <avijer@gmail.com>
2023-11-10 10:33:39 +0100

    Trying solve sort order in TwoSlope: 2

--- kalkayotl/inference.py
+++ kalkayotl/inference.py
@@ -1717,10 +1717,10 @@ class Inference:
 			#-------------------------------------------------
 
 			#------------ Normalizations ------------------------
-			if nvr.min()  < 0 and nvr.max() > 0:
+			if nvr.min() < 0 and nvr.max() > 0:
 				vcenter = 0.0
 			else:
-				vcenter = 0.5*(nvr.max()-nvr.min())
+				vcenter = np.sign(nvr.min())*0.5*np.abs((nvr.max()-nvr.min()))
 
 			print(nvr.min(),vcenter,nvr.max())
 
